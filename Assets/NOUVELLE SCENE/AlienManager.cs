51d79d914f778f0fbec33597d82714291dbca7c8
sexysoushi <barbara1603@hotmail.com>
2017-10-20 02:16:09 -0700

    add nav mesh

---
+++ Assets/NOUVELLE SCENE/AlienManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienManager : MonoBehaviour {
+
+    AvatarController instance;
+    public RandomWalk script;
+
+	// Use this for initialization
+	void Start () {
+        script = GetComponent<RandomWalk>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log("player : " + instance.playerIndex);
+        if (instance.playerIndex != 0)
+        {
+            script.enabled = false;
+        }
+        else
+        {
+            script.enabled = true;
+        }
+    }
+}
